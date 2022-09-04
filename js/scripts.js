34412b91db26dd4fcbba349e812e2b5a1fca1557
Emil Anton <emilbstn@gmail.com>
2022-09-04 04:43:44 -0700

    add a prototype for the second question

--- js/scripts.js
+++ js/scripts.js
@@ -31,3 +31,34 @@ function questionOne(text) {
     return csharpCount;
   }
 }
+
+
+function questionTwo(text) {
+  if (text.trim().length ===0){
+    return 0;
+  }
+  const answer1 = "versatile";
+  const answer2 = "web based";
+  const answer3 = "operations";
+  const textArray = text.split(" ");
+  textArray.forEach(function(element) {
+    if (element.toLowerCase().includes(answer1.toLowerCase())) {
+      pythonCount++;
+    }
+    else if (element.toLowerCase().includes(answer2.toLowerCase())) {
+      jsCount++;
+    }
+    else if (element.toLowerCase().includes(answer3.toLowerCase())) {
+      csharpCount++;
+    }
+  });
+  if (pythonCount === 1) {
+    return pythonCount;
+  }
+  else if (jsCount > pythonCount && jsCount > csharpCount) {
+    return jsCount;
+  }
+  else {
+    return csharpCount;
+  }
+}
