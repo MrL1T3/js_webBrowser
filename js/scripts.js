50d0efa1cc60518662a48ec5388311f832a37f6e
Emil Anton <emilbstn@gmail.com>
2022-09-04 04:41:09 -0700

    add a protoype for first question of survey

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+let pythonCount = 0;
+let jsCount = 0;
+let csharpCount = 0;
+
+function questionOne(text) {
+  if (text.trim().length ===0){
+    return 0;
+  }
+  const answer1 = "easy";
+  const answer2 = "middling";
+  const answer3 = "complex";
+  const textArray = text.split(" ");
+  textArray.forEach(function(element) {
+    if (element.toLowerCase().includes(answer1.toLowerCase())) {
+      pythonCount++;
+    }
+    else if (element.toLowerCase().includes(answer2.toLowerCase())) {
+      jsCount++;
+    }
+    else if (element.toLowerCase().includes(answer3.toLowerCase())) {
+      csharpCount++;
+    }
+  });
+  if (pythonCount === 1) {
+    return pythonCount;
+  }
+  else if (jsCount > pythonCount && jsCount > csharpCount) {
+    return jsCount;
+  }
+  else {
+    return csharpCount;
+  }
+}
