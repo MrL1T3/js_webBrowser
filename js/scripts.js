15d39163d948aaac45002b08bfba7e71afda53b2
Emil Anton <emilbstn@gmail.com>
2022-09-11 03:10:47 -0700

    condense funtiones into single partially working one

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,28 @@ let jsCount = 0;
 let csharpCount = 0;
 
 function questionOne(text) {
+  let answerIndex = 0
   if (text.trim().length ===0){
     return 0;
   }
-  const answer1 = "easy";
-  const answer2 = "middling";
-  const answer3 = "complex";
+  const answer1 = ["easy", "versatile"];
+  const answer2 = ["middling", "website"];
+  const answer3 = ["complex", "operations"];
   const textArray = text.split(" ");
   textArray.forEach(function(element) {
-    if (element.toLowerCase().includes(answer1.toLowerCase())) {
+    if (element.toLowerCase().includes(answer1.at(answerIndex).toLowerCase())) {
       pythonCount++;
+      answerIndex++; 
     }
-    else if (element.toLowerCase().includes(answer2.toLowerCase())) {
+    else if (element.toLowerCase().includes(answer2.at(answerIndex).toLowerCase())) {
       jsCount++;
+      answerIndex++; 
     }
-    else if (element.toLowerCase().includes(answer3.toLowerCase())) {
+    else if (element.toLowerCase().includes(answer3.at(answerIndex).toLowerCase())) {
       csharpCount++;
+      answerIndex++; 
     }
+    
   });
   if (pythonCount > jsCount && pythonCount > csharpCount) {
     return pythonCount;
@@ -27,38 +32,10 @@ function questionOne(text) {
   else if (jsCount > pythonCount && jsCount > csharpCount) {
     return jsCount;
   }
-  else {
+  else if (csharpCount > pythonCount && csharpCount > jsCount) {
     return csharpCount;
   }
-}
-
-
-function questionTwo(text) {
-  if (text.trim().length ===0){
+  else {
     return 0;
-  }
-  const answer1 = "versatile";
-  const answer2 = "web based";
-  const answer3 = "operations";
-  const textArray = text.split(" ");
-  textArray.forEach(function(element) {
-    if (element.toLowerCase().includes(answer1.toLowerCase())) {
-      pythonCount++;
-    }
-    else if (element.toLowerCase().includes(answer2.toLowerCase())) {
-      jsCount++;
-    }
-    else if (element.toLowerCase().includes(answer3.toLowerCase())) {
-      csharpCount++;
-    }
-  });
-  if (pythonCount > jsCount && pythonCount > csharpCount) {
-    return pythonCount;
-  }
-  else if (jsCount > pythonCount && jsCount > csharpCount) {
-    return jsCount;
-  }
-  else {
-    return csharpCount;
   }
 }
